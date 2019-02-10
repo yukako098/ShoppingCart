b7ec4d58c9aa3a8946b7b85b01b1154e7e8d8e75
Yukako Sunabe <yukakosunabe@gmail.com>
2019-02-10 14:56:02 -0800

    Done with putting simple html and css with Bootstrap

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </ul>
+    <ul class="navbar-nav my-2 my-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i> Cart</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user"></i>
+           User
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">User Account</a>
+          <a class="dropdown-item" href="#">Logout</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,131 @@
+@extends('layouts.master')
+
+@section('title')
+    Laravel Shopping Cart
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+            <div class="card-deck">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Product title</h5>
+                        <p class="card-text description">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="price">$12</div>
+                        <a href="#" class="btn btn-success" style="float : right">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+@endsection
